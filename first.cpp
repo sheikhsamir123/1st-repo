d41ec184dfdce018e4f203ea057044b59ad8efd0
sheikh muhammad samir hoda <sheikh_081bct036@tcioe.edu.np>
2025-06-11 23:59:19 +0545

    first commit

---
+++ first.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+//#include<conio.h>
+using namespace std;
+
+class time1{
+private:
+int sec, minute, hr, day;
+
+public:
+void input(){
+    cout<<"enter the value of sec minute hr and day"<<endl;
+    cin>>sec>>minute>>hr>>day;
+}
+
+time1 operator *(time1 t1){
+time1 t;
+t.sec = sec + t1.sec;
+t.minute = minute + t1.minute + t.sec / 60;
+t.hr = hr + t1.hr + t.minute / 60;
+t.day = day + t1.day + t.hr / 24;
+t.sec = t.sec % 60;
+t.minute = t.minute % 60;
+t.hr = t.hr % 24;
+
+return(t);
+}
+
+void display(){
+    cout<<"day="<<day<<"\nhr="<<hr<<"\nminute="<<minute<<"\nsec="<<sec<<endl;
+}
+};
+
+int main(){
+    time1 t2,t3,t4;
+    t2.input();
+    t3.input();
+    t4 = t2*t3;
+    t4.display();
+    cout<<"the value is "<<endl;
+    //getch();
+    return 0;
+}
